6c21f493bf68d27e48f3e7feb11e99168737689e
Captain Isaac A <87286968+CaptainIsaaca@users.noreply.github.com>
2022-09-06 21:19:24 +0200

    Update authenticate.js

--- api/auth/steam/authenticate.js
+++ api/auth/steam/authenticate.js
@@ -3,7 +3,7 @@ const SteamAuth = require("node-steam-openid");
 const url = require('url');
 const codec = require('json-url')('lzw');
 
-const realm = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://invictuslords.com"
+const realm = process.env.URL
 const returnUrl = realm + "/api/auth/steam/authenticate"
 const steam = new SteamAuth({
   realm, // Site name displayed to users on logon
