f71aab935bf1c8cc61300ab1eeb18708912b9785
elrakabawi <mselrakabawi@gmail.com>
2022-09-06 23:38:40 +0200

    Update getRedirect.js

--- api/auth/steam/getRedirect.js
+++ api/auth/steam/getRedirect.js
@@ -1,11 +1,14 @@
 "use strict";
 const SteamAuth = require("node-steam-openid");
 
+const realm = process.env.REALM_URL
+const returnUrl = realm + "/api/auth/steam/authenticate"
 const steam = new SteamAuth({
-  realm: "http://localhost:3000", // Site name displayed to users on logon
-  returnUrl: "http://localhost:3000/api/auth/steam/authenticate", // Your return route
+  realm, // Site name displayed to users on logon
+  returnUrl, // Your return route
   apiKey: process.env.STEAM_API_KEY // Steam API key
 });
+
 
 module.exports = async (req, res) => {
   const redirectUrl = await steam.getRedirectUrl();
