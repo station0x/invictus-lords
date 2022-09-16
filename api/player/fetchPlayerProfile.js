99c3e6dd806240de9f0846544a154eecbd122c37
Captain Isaac A <87286968+CaptainIsaaca@users.noreply.github.com>
2022-09-16 19:06:44 +0200

    Rewards calculation and interval based distrubtion

--- api/player/fetchPlayerProfile.js
+++ api/player/fetchPlayerProfile.js
@@ -1,14 +1,12 @@
 "use strict";
 // Import the dependency.
 const clientPromise = require('../../api-utils/mongodb-client');
-const { ObjectId } = require('mongodb');
 
 module.exports = async (req, res) => {
     const address = req.query.address
     const client = await clientPromise;
     const db = client.db()
     const players = db.collection("players")
-    const query = {address}
     const playerDoc = (await players.find({address}).limit(1).toArray())[0]
     if(!playerDoc) {
         res.status(404).json({ success: false })
