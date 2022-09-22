7948475e76f89e1c419291ed259f122c6aa4dcf0
Captain Isaac A <87286968+CaptainIsaaca@users.noreply.github.com>
2022-09-22 06:54:25 +0200

    hj added

--- src/main.js
+++ src/main.js
@@ -5,11 +5,18 @@ import VueGtag from 'vue-gtag'
 import store from './store'
 import Buefy from 'buefy'
 import VueNumber from 'vue-number-animation'
+import Hotjar from 'vue-hotjar'
 // import 'buefy/dist/buefy.css'
 import '../src/assets/scss/main.scss'
 import './assets/main.css'
 
 Vue.prototype.ethereum = window.ethereum
+
+Vue.use (Hotjar, {
+  id: '3169193',
+  isProduction: true,
+  snippetVersion: 6
+})
 
 Vue.use(Buefy)
 Vue.use(VueNumber)
